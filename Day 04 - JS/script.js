b208dba198b3c443c49aee8232fcd96fd240110a
Huyennkfeb <huyennk.feb@gmail.com>
2023-04-12 16:30:09 +0700

    baitap nhung JS

---
+++ Day 04 - JS/script.js
@@ -0,0 +1,7 @@
+// Khai bao bien
+// let x,y,z;
+//alert('xin chao');
+//prompt('nhap ten cua ban');
+//confirm('ban co muon xoa san pham nay khong');
+document.write('hello world');
+console.log('xin chao');
