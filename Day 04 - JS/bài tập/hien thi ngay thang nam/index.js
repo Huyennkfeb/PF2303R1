a4e75ae610abb8853abb9b96381dbf860c7519eb
Huyennkfeb <huyennk.feb@gmail.com>
2023-06-02 23:50:57 +0700

    hien thi ngay thang nam

---
+++ Day 04 - JS/bài tập/hien thi ngay thang nam/index.js
@@ -0,0 +1,8 @@
+let inputDay;
+let inputMonth;
+let inputYear;
+inputDay = prompt('Enter the day')
+inputMonth = prompt('Enter the month')
+inputYear = prompt('Enter the year')
+let date = inputDay +'-' +inputMonth + '-' + inputYear;
+document.write(date);
