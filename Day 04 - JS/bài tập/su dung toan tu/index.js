10f18647c0e72882a1f5b36b5b7d88c32a85a9b8
Huyennkfeb <huyennk.feb@gmail.com>
2023-06-02 23:38:19 +0700

    thuc hanh su dung toan tu

---
+++ Day 04 - JS/bài tập/su dung toan tu/index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("enter the width");
+inputHeight = prompt('enter the height');
+let witdh = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = witdh * height;
+document.write('The area is :' + area );
